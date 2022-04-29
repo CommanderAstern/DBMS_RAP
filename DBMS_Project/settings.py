ec359dd03fe26b90b085dcb1731453f7813eaa2c
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2022-04-29 21:39:03 +0530

    deleted dummy model

--- DBMS_Project/settings.py
+++ DBMS_Project/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'fir',
     'station',
     'users',
-    'dummy',
 ]
 
 MIDDLEWARE = [
--- DBMS_Project/views.py
+++ DBMS_Project/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from users import models as user_models
-from dummy import models as dummy_models
 from django.http import HttpResponse
 import numpy as np
 import random as rand
@@ -8,25 +7,25 @@ import random as rand
 def homepage(request):
     return render(request, 'homepage/homepage.html')
 
-def file_an_fir(request):
-    if request.method == 'GET':
-        return render(request, 'form/form.html')
+# def file_an_fir(request):
+#     if request.method == 'GET':
+#         return render(request, 'form/form.html')
 
-    if request.method == 'POST':
-        print(request.POST)
-        f = dummy_models.form()
-        f.first_name = request.POST['first_name']
-        f.last_name = request.POST['last_name']
-        f.email = request.POST['email']
-        f.phone = request.POST['phone']
-        f.birth_date = request.POST['birthdate']
-        f.details = request.POST['details']
-        f.suspects = request.POST['suspect']
-        f.save()
+#     if request.method == 'POST':
+#         print(request.POST)
+#         f = dummy_models.form()
+#         f.first_name = request.POST['first_name']
+#         f.last_name = request.POST['last_name']
+#         f.email = request.POST['email']
+#         f.phone = request.POST['phone']
+#         f.birth_date = request.POST['birthdate']
+#         f.details = request.POST['details']
+#         f.suspects = request.POST['suspect']
+#         f.save()
 
-        print(f)
+#         print(f)
 
-        return HttpResponse('Form submitted successfully')
+#         return HttpResponse('Form submitted successfully')
 
 def faqs(request):
     return render(request, 'faqs/faqs.html')
