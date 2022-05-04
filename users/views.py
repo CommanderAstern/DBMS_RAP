cc9b3fe4e6f50b695cf073e46d5b025115ebf6f7
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2022-05-05 00:08:36 +0530

    css changes in the police dashboard

--- users/views.py
+++ users/views.py
@@ -1,5 +1,3 @@
-from unicodedata import category
-from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
