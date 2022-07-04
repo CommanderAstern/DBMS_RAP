c8a33c56a989039863251c2b769e86dc1fa88f02
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2022-07-04 18:57:19 +0530

    Revert "Updating the main branch"

--- station/models.py
+++ station/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Station(models.Model):
-    name = models.CharField(max_length = 100)
-    address = models.CharField(max_length = 100)
-    status = models.CharField(max_length = 20)
+    station_id = models.CharField(max_length = 10, primary_key = True)
+    station_name = models.CharField(max_length = 100)
+    station_address = models.CharField(max_length = 100)
+    station_status = models.CharField(max_length = 20)
 
     def __str__(self):
-        return self.name
+        return self.station_id
